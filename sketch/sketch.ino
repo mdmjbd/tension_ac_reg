03f747a4fcc4a3db39c52f364ec2517449ff949a
mdmjbd <mdmjbd@gmail.com>
2023-08-05 12:43:43 -0400

    Commit

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,9 +1,30 @@
+#include <ZMPT101B.h>
+
+#define VOLTAGE_SENSOR_PIN A0
+
+ZMPT101B voltageSensor(VOLTAGE_SENSOR_PIN);
+
+
 void setup() {
+  Serial.begin(9600);
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Set zero point while sensor is live
+  voltageSensor.calibrateLive();
+
+  // To measure the voltage we need to know the frequency
+  // By default 50Hz is used, but you can specify the desired frequency
+  // as the first argument for "getVoltageAC()"
+  float V = voltageSensor.getVoltageAC();
+
+  Serial.println(String(V) + " V");
+
+  delay(1000);
 
 }
+
+
+
